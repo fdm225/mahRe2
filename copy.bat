820f55547541751cfa4d8333a9f3f5bdca67fd0f
david morrison <dmorrison@versa-networks.com>
2023-04-03 06:07:48 -0500

    - updated to remove the logs directory

--- copy.bat
+++ copy.bat
@@ -1,3 +1,4 @@
 del C:\Users\david\OneDrive\Documents\EdgeTX\horus_sdcard\WIDGETS\mahRe2\*.lua
 del C:\Users\david\OneDrive\Documents\EdgeTX\horus_sdcard\WIDGETS\mahRe2\*.luac
+del C:\Users\david\OneDrive\Documents\EdgeTX\horus_sdcard\WIDGETS\mahRe2\logs\*.log
 copy C:\Users\david\PycharmProjects\mahRe2\*.lua C:\Users\david\OneDrive\Documents\EdgeTX\horus_sdcard\WIDGETS\mahRe2\
